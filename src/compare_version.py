94fdc3b8ac6762df0dee4b12f5ea4930f8199617
Itay Shoshani <5647956+itay-sho@users.noreply.github.com>
2023-09-22 18:12:51 -0400

    fixing len with is not None

--- src/compare_version.py
+++ src/compare_version.py
@@ -50,8 +50,8 @@ def main():
 
     args = parse_arguments()
     pkg_name = args.pkg_name
-    local_pkg_name = args.local_pkg_name if len(args.local_pkg_name) > 0 else pkg_name
-    pypi_pkg_name = args.pypi_pkg_name if len(args.pypi_pkg_name) > 0 else pkg_name
+    local_pkg_name = args.local_pkg_name if args.local_pkg_name is not None else pkg_name
+    pypi_pkg_name = args.pypi_pkg_name if args.pypi_pkg_name is not None else pkg_name
 
     if not pkg_name:  # pkg name and module names should match if missing
         pkg_name = find_pkg_name()
